3c31a4ddb0f010a309e96efa23ca5c0fae21eae7
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2022-03-16 15:42:46 +0900

    create detail section

---
+++ src/js/data.js
@@ -0,0 +1,23 @@
+export const saunaData = {
+  text:'Saunaテキストられぽばのぅらねだゃちおぬせぷつけおなわそよょよごばたぉれゆえぢぱこしおゐへぜごだてめじすぷみぉふいげずあせだきぺもをきつぅゐほすざよにぉぉぜとわぁにあさ',
+  title:'Sauna',
+  image:'../images/sauna.png',
+  icon:'../images/sauna_icon.png',
+  button:'view more details'
+}
+
+export const foodData = {
+  text:'Foodテキストぎどとなすぜぜぴぴぶびぅゐぷだどみばぷぇごがこうゐうごぶろろよゑびぷかゑせてそたなたちでてそょゕゆろひへがょうなぢうせおゅぅにくがんげいちすゅもほりぁぉゐよぉよ',
+  title:'Food',
+  image:'../images/food.png',
+  icon:'../images/food_icon.png',
+  button:'All Menu'
+}
+
+export const goodsData = {
+  text:'Goodsテキストぎどとなすぜぜぴぴぶびぅゐぷだどみばぷぇごがこうゐうごぶろろよゑびぷかゑせてそたなたちでてそょゕゆろひへがょうなぢうせおゅぅにくがんげいちすゅもほりぁぉゐよぉよ',
+  title:'Goods',
+  image:'../images/goods.png',
+  icon:'../images/goods_icon.png',
+  button:'All Goods'
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,33 @@
 import "../scss/style.scss";
+import { saunaData, foodData, goodsData } from "./data";
 
-const text = document.getElementById("test");
-const check = document.getElementsByName("check");
-const length = check.length;
-for (let i = 0; i < length; i++) {
-  const value = check[i].value;
-    text.innerHTML = "saunaです";
-  check[i].addEventListener('change', () => {
-    if(value === "sauna") {
-      text.innerHTML = "saunaです";
+const width = window.innerWidth <= 768
+const title = document.getElementById("title");
+const image = document.getElementById("image");
+const iconImage = document.getElementById("icon-image");
+const text = document.getElementById("text");
+const button = width ? document.getElementById("sp-button") : document.getElementById('pc-button')
+const radio = document.getElementsByName("choise");
+let val = "";
+
+const choiseDetail = (value) => {
+  title.innerHTML = value.title;
+  text.innerHTML = value.text;
+  image.setAttribute("src", value.image);
+  iconImage.setAttribute("src", value.icon);
+  button.textContent = value.button;
+  console.log(value.image);
+};
+
+for (const btn of radio) {
+  btn.addEventListener("change", () => {
+    if (btn.checked === true) {
+      val = btn.value;
+      if (val === "sauna")return choiseDetail(saunaData);
+      if (val === "food") return choiseDetail(foodData);
+      if (val === "goods") return choiseDetail(goodsData);
+      if (val === "") return;
     }
-    if(value === "food") {
-      text.innerHTML = "foodです";
-    }
-    if(value === "goods") {
-      text.innerHTML = "goodsです";
-    }
-  })
+  });
 }
+
