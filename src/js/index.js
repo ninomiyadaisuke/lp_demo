5a9d23a0976d38bb2360195fe26c8f76dc5d8ea1
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2022-03-25 09:45:24 +0900

    create contact section

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,15 @@ const image = document.getElementById("image");
 const iconImage = document.getElementById("icon-image");
 const text = document.getElementById("text");
 const radio = document.getElementsByName("choise");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputSubject = document.getElementById("subject");
+const inputMessage = document.getElementById("message");
+const contactButton = document.getElementById("contact-button");
+const inputCheck = document.getElementById("check");
 let val = "";
 
-const choiseDetail = (value ) => {
+const choiseDetail = (value) => {
   title.innerHTML = value.title;
   text.innerHTML = value.text;
   image.setAttribute("src", value.image);
@@ -22,7 +28,7 @@ for (const btn of radio) {
   btn.addEventListener("change", () => {
     val = btn.value;
     if (btn.checked === true) {
-      if (val === "sauna")return choiseDetail(saunaData);
+      if (val === "sauna") return choiseDetail(saunaData);
       if (val === "food") return choiseDetail(foodData);
       if (val === "goods") return choiseDetail(goodsData);
       if (val === "") return;
@@ -30,15 +36,40 @@ for (const btn of radio) {
   });
 }
 
-window.onload = function () {
-  var nav = document.getElementById('nav-wrapper');
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
-
-  hamburger.addEventListener('click', function () {
-      nav.classList.toggle('open');
-  });
-  blackBg.addEventListener('click', function () {
-      nav.classList.remove('open');
-  });
-};
+contactButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (inputCheck.checked) {
+    if (inputName.value === "") {
+      inputName.classList.add("form-invalid");
+      return;
+    } else {
+      inputName.classList.remove("form-invalid");
+    }
+    if (inputEmail.value === "") {
+      inputEmail.classList.add("form-invalid");
+      return;
+    } else {
+      inputEmail.classList.remove("form-invalid");
+    }
+    if (inputSubject.value === "") {
+      inputSubject.classList.add("form-invalid");
+      return;
+    } else {
+      inputSubject.classList.remove("form-invalid");
+    }
+    if (inputSubject.value === "") {
+      inputMessage.classList.add("form-invalid");
+      return;
+    } else {
+      inputMessage.classList.remove("form-invalid");
+    }
+    const contact = {
+      name: inputName.value,
+      email: inputEmail.value,
+      subject: inputSubject.value,
+      message: inputMessage.value,
+    };
+  } else {
+    alert('チェックを入れてください')
+  }
+});
